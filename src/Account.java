1c054de8470c7e90a3cae5a3406050b84348fbb3
karlminator <kf.holmsten@gmail.com>
2025-01-10 11:10:01 +0100

    Added account numbers.

--- src/Account.java
+++ src/Account.java
@@ -1,11 +1,21 @@
 public abstract class Account {
     private double balance;
     private long accountNo;
+    private static long salaryCounter = 1000;
+    private static long savingsCounter = 2000;
 
 
     public Account(long accountNo) {
         this.balance = 500.0;  // Alla nya konton börjar med 500 i saldo
         this.accountNo = accountNo;
+    }
+
+    protected static long genSalAccNo() {
+        return salaryCounter++;
+    }
+
+    protected static long genSavAccNo() {
+        return savingsCounter++;
     }
 
     // Getters
